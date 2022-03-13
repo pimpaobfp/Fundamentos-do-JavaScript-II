adf9987612cd8014e022ec9ed138204fd2e0fb72
Bruno <brunobothp@gmail.com>
2022-03-12 21:50:56 -0300

    Trabalhando com .reduce

---
+++ media-geral.js
@@ -0,0 +1,18 @@
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava  = [6, 5, 8, 9, 5, 6];
+const salaPython = [7, 3.5, 8, 9.5];
+
+function mediaSala(notasDaSala) {
+    const somaDasNotas = notasDaSala.reduce((acum, atual) => atual + acum, 0)
+    return somaDasNotas / notasDaSala.length
+}
+
+console.log(`Media das salas de JavaScript ${mediaSala(salaJS)}`);
+console.log(`Media das salas de JavaScript ${mediaSala(salaJava)}`);
+console.log(`Media das salas de JavaScript ${mediaSala(salaPython)}`);
+
+const notas = [10, 6.5, 8, 7];
+
+const media = notas.reduce((acum, atual) => atual + acum, 0) / notas.length;
+
+console.log(media);
